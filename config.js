ce99cd64cf73047b799cbf7e4d8d49e5dd7ba7b4
ojuolokun86 <ojuolokun86@gmail.com>
2025-05-20 05:32:30 +0100

    NEW STYLE IN FRONT PAGE And font ad

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-//const URL = 'https://m-bot-production-84d7.up.railway.app';
+const URL = 'https://bmm-production.up.railway.app';
 //const URL = 'http://localhost:3000'; // Replace with your actual URL
-const URL = 'https://m-bot.fly.dev';
+//const URL = 'https://m-bot.fly.dev';
 
 
 
